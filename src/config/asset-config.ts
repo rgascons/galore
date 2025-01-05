fbccc0e17c138d4512bf94cd98ed0aaef0fce33d
Ricard Gascons <gascons1995@gmail.com>
2025-01-05 17:12:10 -0600

    update config to support base_url

--- src/config/asset-config.ts
+++ src/config/asset-config.ts
@@ -1,10 +1,12 @@
+import { BASE_DIR } from '../../constants';
+
 export const Assets = {
   // Specify your asset paths and keys
   Images: {
-    Player: '/assets/player.png',
-    Wall: '/assets/wall.png',
-    Bullet: '/assets/bullet.png',
-    Monster: '/assets/monster.png',
+    Player: BASE_DIR + '/assets/player.png',
+    Wall: BASE_DIR + '/assets/wall.png',
+    Bullet: BASE_DIR + '/assets/bullet.png',
+    Monster: BASE_DIR + '/assets/monster.png',
     // Add more assets as needed
   },
   // Add more asset types (audio, spritesheets, etc.) as needed
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import path from 'path';
+import { BASE_DIR } from './constants';
 
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
   server: {
     port: 3000,
   },
-  base: './',
+  base: BASE_DIR,
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
